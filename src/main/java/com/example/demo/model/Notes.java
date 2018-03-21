b0dbcb1fef18d52ab510512a2e2e7b5cc909a1cf
desiCoda <j.pushkarsingh@gmail.com>
2018-03-21 22:26:03 +0530

    added model classes for Recipe and Notes entities.

---
+++ src/main/java/com/example/demo/model/Notes.java
@@ -0,0 +1,45 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Notes {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long notesID;
+	@Lob //now user can bypass the hibernate's default limit of 255 chars.
+	private String recipeNotes; //will be saved as a CLOB
+	@OneToOne // no cascading as we have designed Recipe to be the owner of this bean. If we
+				// decide to delete notes then that would not impact Recipe object.
+	private Recipe recipe;
+
+	public String getRecipeNotes() {
+		return recipeNotes;
+	}
+
+	public void setRecipeNotes(String recipeNotes) {
+		this.recipeNotes = recipeNotes;
+	}
+
+	public Long getNotesID() {
+		return notesID;
+	}
+
+	public void setNotesID(Long notesID) {
+		this.notesID = notesID;
+	}
+
+	public Recipe getRecipe() {
+		return recipe;
+	}
+
+	public void setRecipe(Recipe recipe) {
+		this.recipe = recipe;
+	}
+}
---
+++ src/main/java/com/example/demo/model/Recipe.java
@@ -0,0 +1,102 @@
+package com.example.demo.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Recipe {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // underlying pa would generate the unique id sequence
+														// accordingly
+	private Long recipeID;
+
+	private Integer prepTime;
+	private Integer cookTime;
+	private Integer servings;
+	private String url;
+	private String directions;
+	private String description;
+	@Lob
+	private Byte[] image;  //will be stored as a BLOB in db
+	@OneToOne(cascade = CascadeType.ALL) // owner entity for Notes. If any state change happens for Recipe then Notes
+											// would get that.
+	private Notes notes;
+
+	public Long getRecipeID() {
+		return recipeID;
+	}
+
+	public void setRecipeID(Long recipeID) {
+		this.recipeID = recipeID;
+	}
+
+	public Integer getPrepTime() {
+		return prepTime;
+	}
+
+	public void setPrepTime(Integer prepTime) {
+		this.prepTime = prepTime;
+	}
+
+	public Integer getCookTime() {
+		return cookTime;
+	}
+
+	public void setCookTime(Integer cookTime) {
+		this.cookTime = cookTime;
+	}
+
+	public Integer getServings() {
+		return servings;
+	}
+
+	public void setServings(Integer servings) {
+		this.servings = servings;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getDirections() {
+		return directions;
+	}
+
+	public void setDirections(String directions) {
+		this.directions = directions;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Byte[] getImage() {
+		return image;
+	}
+
+	public void setImage(Byte[] image) {
+		this.image = image;
+	}
+
+	public Notes getNotes() {
+		return notes;
+	}
+
+	public void setNotes(Notes notes) {
+		this.notes = notes;
+	}
+}
