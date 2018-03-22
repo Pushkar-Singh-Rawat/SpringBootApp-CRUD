bd74ab514bcf24bda7e24c26f154e85ed2e361d5
desiCoda <j.pushkarsingh@gmail.com>
2018-03-22 12:31:05 +0530

    added CRUD repositories for Category,Recipe and unitofmeasurement entities

---
+++ src/main/java/com/example/demo/respositories/UnitOfMeasurementRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.respositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo.model.UnitOfMeasurement;
+
+public interface UnitOfMeasurementRepository extends CrudRepository<UnitOfMeasurement, Long> {
+
+}
