53dcfd0853f2dda3a36cf671c9484ae1c306a3ff
desiCoda <j.pushkarsingh@gmail.com>
2018-03-22 14:06:20 +0530

    updated CategoryRepo and UnitOfMeasurementRepo with 'findBy' method signatures.

--- src/main/java/com/example/demo/respositories/CategoryRepository.java
+++ src/main/java/com/example/demo/respositories/CategoryRepository.java
@@ -3,7 +3,12 @@ package com.example.demo.respositories;
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.demo.model.Category;
+import java.lang.String;
+import java.util.List;
+import java.util.Optional;
 
 public interface CategoryRepository extends CrudRepository<Category, Long>{
 
+ Optional<Category> findByDescription(String description);
+ 
 }
