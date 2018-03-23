3bd85182ef36e263904ac917156a854ce3554145
desiCoda <j.pushkarsingh@gmail.com>
2018-03-23 14:36:21 +0530

    updated HomeController test to include test step for verfying method interactions within HomeController.java class

--- src/test/java/com/example/demo/controller/HomeControllerTest.java
+++ src/test/java/com/example/demo/controller/HomeControllerTest.java
@@ -3,6 +3,10 @@ package com.example.demo.controller;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -11,6 +15,7 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
 import com.example.demo.controllers.HomeController;
+import com.example.demo.model.Recipe;
 import com.example.demo.services.RecipeService;
 
 public class HomeControllerTest {
@@ -19,7 +24,7 @@ public class HomeControllerTest {
 	RecipeService recipeService;
 	HomeController homeController;
 	@Mock
-	Model model;
+	Model model; // mocked Model interface
 
 	@Before
 	public void setup() throws Exception {
@@ -30,10 +35,23 @@ public class HomeControllerTest {
 
 	@Test
 	public void getHome() {
-		
-		assertEquals(homeController.getHome(model),"homePage");
-		verify(recipeService,times(1)).getRecipe();
-		verify(recipeService,times(0)).getRecipe();
-		verify(recipeService,times(2)).getRecipe();
+		String homePageNameActual = homeController.getHome(model);
+		String homePageNameExpected = "homePage";
+		Set<Recipe> recipes = new HashSet<>();
+		recipes.add(new Recipe());
+		Recipe recipe = new Recipe();
+		recipe.setRecipeID(1L);
+		recipes.add(recipe);
+
+		when(recipeService.getRecipe()).thenReturn(recipes);
+		assertEquals(homePageNameExpected, homePageNameActual);
+
+		verify(recipeService, times(1)).getRecipe();
+		verify(model, times(1)).addAttribute("recipes", recipeService.getRecipe());
+		/*
+		 * verify(recipeService,times(0)).getRecipe();
+		 * verify(recipeService,times(2)).getRecipe();
+		 */
+
 	}
 }
