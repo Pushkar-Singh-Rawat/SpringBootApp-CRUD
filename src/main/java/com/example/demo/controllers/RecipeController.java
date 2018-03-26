52c12a4f15ae9888735bde4e79c328db2b1b9abb
desiCoda <j.pushkarsingh@gmail.com>
2018-03-26 13:47:04 +0530

    added request mapping for rendering recipeform.html in RecipeController.java. method added: newRecipe() and saveOrUpdateRecipe()

--- src/main/java/com/example/demo/controllers/RecipeController.java
+++ src/main/java/com/example/demo/controllers/RecipeController.java
@@ -3,9 +3,12 @@ package com.example.demo.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.commands.RecipeCommand;
 import com.example.demo.services.RecipeService;
 
 @Controller
@@ -13,15 +16,29 @@ public class RecipeController {
 
 	private RecipeService recipeService;
 
-	@Autowired // just to indicate that springcontext injects RecipeServiceImpl object here
+	@Autowired // just to indicate that springcontext injects RecipeServiceImpl
+				// object here
 	public RecipeController(RecipeService recipeService) {
 		this.recipeService = recipeService;
 	}
 
 	@RequestMapping("/recipe/show/{id}") // setting up MVC.
-	public String showById(Model model,@PathVariable String id) {
+	public String showById(Model model, @PathVariable String id) {
 		model.addAttribute("recipe", recipeService.findById(new Long(id)));
-		return "recipe/show"; 
+		return "recipe/show";
 	}
 
+	@RequestMapping("/recipe/new")
+	public String newRecipe(Model model) {
+		model.addAttribute("recipe", new RecipeCommand());
+		return "recipe/recipeform";
+	}
+
+	@PostMapping
+	@RequestMapping("recipe")
+	public String saveOrUpdateRecipe(@ModelAttribute RecipeCommand command) {
+		// @ModalAttribute grabs any post object present in the "recipe" path.
+		RecipeCommand savedRecipe = recipeService.saveRecipeCommand(command);
+		return "redirect:/recipe/show/" + savedRecipe.getRecipeID();
+	}
 }
