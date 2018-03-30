8468fe46bd4ea246f9db13c057105d510cc57caa
desiCoda <j.pushkarsingh@gmail.com>
2018-03-31 01:19:10 +0530

    added @GetMapping annotation and log.debug for deleteRecipe() handler method in RecipeController.java

--- src/main/java/com/example/demo/controllers/RecipeController.java
+++ src/main/java/com/example/demo/controllers/RecipeController.java
@@ -3,6 +3,7 @@ package com.example.demo.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,7 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.commands.RecipeCommand;
 import com.example.demo.services.RecipeService;
+import com.example.demo.services.RecipeServiceImpl;
 
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
 @Controller
 public class RecipeController {
 
@@ -22,18 +26,20 @@ public class RecipeController {
 		this.recipeService = recipeService;
 	}
 
+	@GetMapping
 	@RequestMapping("/recipe/{id}/show") // setting up MVC.
 	public String showById(Model model, @PathVariable String id) {
 		model.addAttribute("recipe", recipeService.findById(new Long(id)));
 		return "recipe/show";
 	}
-
+	@GetMapping
 	@RequestMapping("/recipe/new")
 	public String newRecipe(Model model) {
 		model.addAttribute("recipe", new RecipeCommand());
 		return "recipe/recipeform";
 	}
 
+	@GetMapping
 	@RequestMapping("/recipe/{id}/update") // setting up MVC.
 	public String updateRecipe(Model model, @PathVariable String id) {
 		model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
@@ -48,8 +54,10 @@ public class RecipeController {
 		return "redirect:/recipe/" + savedRecipe.getRecipeID() + "/show";
 	}
 
+	@GetMapping
 	@RequestMapping("/recipe/{id}/delete") // setting up MVC.
 	public String deleteRecipe(@PathVariable String id) {
+		log.debug("id of recipe to be deleted "+ id);
 		recipeService.deleteById(Long.valueOf(id));
 		return "redirect:/";
 	}
